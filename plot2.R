3cd513e62e9321aa6f02462a85645727454fda88
schloby <sarah.b.schlobohm@gmail.com>
2016-04-13 04:09:32 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,8 +1,8 @@
 
 #Be in the right directory and download and unzip the file if it doesn't exist
 
-setwd("~/data science coursework/Lesson_4_Exploratory_Data_Analysis/Course_Project_1")
-
+#setwd("~/data science coursework/Lesson_4_Exploratory_Data_Analysis/Course_Project_1")
+#edit for you own WD if you need to change it
 
 if  (!file.exists("household_power_consumption.txt")){
   url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
